4d79bcbe668307acc3f9c4d4c1e582e964ea9e20
Gaierte <2312713248@qq.com>
2020-11-15 02:38:00 +0800

    feat:...

---
+++ src/pages/Home/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="header">
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+.header{
+    width: 100%;
+<<<<<<< HEAD
+    height: ;
+=======
+    height: 50px;
+>>>>>>> 3b175b2e9c4a299795fe233fab78f29ca6509d71
+
+}
+</style>
