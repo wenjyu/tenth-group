78aabc8e0f115d4ff8eac599ee3d05841d9a086e
Gaierte <2312713248@qq.com>
2020-11-15 02:49:21 +0800

    feat:...

---
+++ src/pages/Seconds kill/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="tabnavtwo">
+    <span>首页</span>
+    <i class="fa fa-chevron-right" aria-hidden="true"></i>
+     <span>秒杀</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name:"Seconds kill"
+}
+</script>
+
+<style>
+
+</style>
